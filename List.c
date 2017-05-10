99f21f4f3002c579d27780c66679f01ff82b09f5
Gervaise Alina <gervaisita@gmail.com>
2017-05-10 14:08:07 +0200

    Add C file

---
+++ List.c
@@ -0,0 +1,152 @@
+//
+// Created by Gervaise ALINA on 5/10/2017.
+//
+/********************************************************************
+ *
+ * $Id: List.h 1989 2015-10-16 18:19:52Z phil $
+ *
+ ********************************************************************
+ */
+/**
+ *
+ * Creation of a generic (simply linked) List structure.
+ *
+ * To create a list, one must provide two functions (one function to
+ * compare / order elements, one function to display them). Unlike arrays,
+ * indices begins with 1.
+ *
+ ********************************************************************/
+
+
+#include <stdlib.h>
+#include "List.h"
+#include "status.h"
+
+
+
+/** Comparison function for list elements.
+ * Must follow the "strcmp" convention: result is negative if e1 is less
+ * than e2, null if they are equal, and positive otherwise.
+ */
+typedef int (*compFun)   (void* e1, void* e2){
+
+}
+
+/** Display function for list elements */
+typedef void(*prFun)   (void* L){
+
+}
+
+
+/** Empty List creation by dynamic memory allocation (O(1)).
+ * @param comp comparison function between elements (ala strcmp())
+ * @param pr display function for list elements
+ * @return a new (empty) list if memory allocation OK
+ * @return 0 otherwise
+ */
+List*	newList	(compFun comp, prFun pr){
+
+}
+
+/** destroy the list by deallocating used memory (O(N)).
+ * @param l the list to destroy */
+void 	delList	(List* L){
+
+}
+
+/** get the Nth element of the list (O(N)).
+ * @param l the list
+ * @param n the index of the element in list
+ * @param e (out) the searched element
+ * @return OK if element found
+ * @return ERRINDEX if index out of list bounds
+ */
+status 	nthInList	(List* L, int p, void** e){
+
+}
+
+/** Insert element at a given position in the list (O(N)).
+ * @param l the list to store the element in
+ * @param p the position of the insertion point
+ * @param e the element to insert
+ * @return ERRINDEX if position is out of list bounds
+ * @return ERRALLOC if memory allocation failed
+ * @return OK otherwise
+ */
+status 	addListAt	(List*L, int p, void* e){
+
+}
+
+/** remove the element located at a given position in list (O(N)).
+ * @param l the list to remove the element from
+ * @param p the position of the element to remove
+ * @param e (out) the removed element
+ * @return ERRINDEX if position is out of list bounds
+ * @return OK otherwise
+ */
+status 	remFromListAt	(List* L, int p,void** e){
+
+}
+
+/** remove given element from given list (O(N)).
+ * implies the user has given a comparison function.
+ * @param l the list to remove the element from
+ * @param e the element to remove
+ * @return ERRABSENT if element is not found in list
+ * @return ERRUNABLE if no comparison function has been provided
+ * @return OK otherwise
+ */
+status 	remFromList	(List*L, void*e){
+
+}
+
+/** display list elements as "[ e1 ... eN ]" (O(N)).
+ * The display of the element is delegated to the prFun function
+ * @param l the list to display.
+ * @return ERRUNABLE if no comparison function has been provided
+ * @return OK otherwise
+*/
+status 	displayList	(List* L){
+
+}
+
+/** sequencially call given function with each element of given list (O(NxF)).
+ * @param l the list
+ * @param f the function
+ */
+void	forEach		(List* L, void(*)(void*)){
+
+}
+
+/** compute and return the number of elements in given list (O(1)).
+ * @param l the list
+ * @return the number of elements in given list
+ */
+int	lengthList	(List* L){
+
+}
+
+/** add given element to given list according to compFun function (O(N)).
+ * @param l the list (supposedly sorted according to compFun function)
+ * @param e the element to add
+ * @return ERRALLOC if memory allocation failed
+ * @return ERRUNABLE if no comparison function has been provided
+ * @return OK otherwise
+ */
+status	addList	(List*L, void*e){
+
+}
+
+/** tests whether the list contains given element (O(N)).
+ * @param l the list
+ * @param e the searched element
+ * @return 0 if element is not found in list
+ * @return 1 if element is at the head of the list (no predecessor)
+ * @return (a pointer to) the predecessor of the search element otherwise
+ */
+Node*	isInList	(List*L, void*e){
+
+}
+
+
+
